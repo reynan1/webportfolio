dc9099a6c7f42f931fdc019b6831fff458a3b3fd
reynan.delizo <nandelizo2@gmail.com>
2022-10-12 01:26:13 +0800

    adding mobile navigation bar

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -3,7 +3,7 @@ const scrollBar = () => {
   let header1 = document.querySelector("#my-header1-id");
   let header2 = document.querySelector("#my-header2-id");
   let logoImg = document.getElementById('logo-img-id');
-  let toggle = document.getElementById('logo-togle-id');
+  let toggle = document.querySelector('.logo-togle-id');
   window.addEventListener("scroll", () => {
     let scrollBarMovement =
       window.pageYOffset || document.documentElement.scrollBarMovement;
@@ -28,9 +28,29 @@ const scrollBar = () => {
 
 scrollBar();
 
-document.getElementById('toggle-btn').addEventListener('click', () => {
-    let navigationList = document.getElementById('navigation-id')
+document.querySelector('.toggle-btn').addEventListener('click', () => {
+    let navigation = document.querySelector(".navigation");
+    let closeIcon = document.querySelector('.navigation__close-togle');
+    let closeBtn = document.querySelector('.close-btn-id');
+    let toggleBtn = document.querySelector('.toggle-btn');
+    let navigationList = document.querySelector('.navigation-id');
     navigationList.style.setProperty('display', 'block', 'important');
-    navigationList.style.setProperty('width', '100%', 'important');
-    navigationList.style.setProperty('border', '2px solid red', 'important');
+    navigationList.style.setProperty('height', '100%', 'important');
+    navigationList.style.setProperty('background-color', 'rgba(7, 0, 0, 0.9)', 'important');
+    navigation.style.setProperty('background-color', 'rgba(7, 0, 0, 0.9)', 'important');
+    navigation.style.setProperty('height', '100vh', 'important');
+    toggleBtn.style.display = "none";
+    closeBtn.style.display = "block";
+    closeIcon.style.display = "block";
 })
+
+ document.querySelector('.close-btn-id').addEventListener('click', () => {
+  let navigation = document.querySelector(".navigation");
+  let toggleBtn = document.querySelector('.toggle-btn');
+  let navigationList = document.querySelector('.navigation-id');
+  navigation.style.setProperty('background-color', '#2a2b2c', 'important');
+  navigation.style.setProperty('height', '5vh', 'important');
+  navigationList.style.display = "none";
+  toggleBtn.style.display = "block";
+  document.querySelector('.close-btn-id').style.display = "none";
+});
