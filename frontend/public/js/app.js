5fb0aabeab9f6a52a200248e62e1be921fc0eb0a
reynan.delizo <nandelizo2@gmail.com>
2022-10-11 23:32:44 +0800

    adding mobile layout

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -3,10 +3,10 @@ const scrollBar = () => {
   let header1 = document.querySelector("#my-header1-id");
   let header2 = document.querySelector("#my-header2-id");
   let logoImg = document.getElementById('logo-img-id');
+  let toggle = document.getElementById('logo-togle-id');
   window.addEventListener("scroll", () => {
     let scrollBarMovement =
       window.pageYOffset || document.documentElement.scrollBarMovement;
-    console.log(scrollBarMovement);
     if (scrollBarMovement > webScrollBar) {
       header2.style.setProperty('display', 'block', 'important');
       header2.style.setProperty('display', 'block', 'important');
@@ -17,6 +17,7 @@ const scrollBar = () => {
       header1.style.setProperty('display', 'block', 'important');
       header2.style.opacity = "100";
       header2.style.setProperty('display', 'none', 'important');
+      toggle.style.setProperty('background-color', 'rgba(7, 0, 0, 0.9)', 'important');
       logoImg.classname = 'navigation__logo1';
       console.log("header2")
     }
@@ -26,3 +27,10 @@ const scrollBar = () => {
 };
 
 scrollBar();
+
+document.getElementById('toggle-btn').addEventListener('click', () => {
+    let navigationList = document.getElementById('navigation-id')
+    navigationList.style.setProperty('display', 'block', 'important');
+    navigationList.style.setProperty('width', '100%', 'important');
+    navigationList.style.setProperty('border', '2px solid red', 'important');
+})
