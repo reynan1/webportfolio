aa8218c13137fadfb5c4c3845de1a6cf31132e12
reynan.delizo <nandelizo2@gmail.com>
2022-10-24 22:10:44 +0800

    adding navigation fixed position with sticky animation only trigger using scroll bar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
 
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove("scroll-up");
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-up");
+		body.classList.add("scroll-down");
+	} else if (
+		currentScroll < lastScroll &&
+		body.classList.contains("scroll-down")
+	) {
+		body.classList.remove("scroll-down");
+		body.classList.add("scroll-up");
+	}
+	lastScroll = currentScroll;
+});
+
+
+
 function openNav() {
   let element = document.getElementById("header-nav");
   element.classList.add("nav-open");
